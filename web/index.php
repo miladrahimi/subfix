64741c1d1e557a98c3eca2e7ed7f3da7c484b73d
Milad Rahimi <info@miladrahimi.com>
2021-07-11 16:11:20 +0430

    update

--- web/index.php
+++ web/index.php
@@ -1,25 +1,47 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Subfix | Online Persian Subtitle Fixer</title>
+    <meta name="description" content="A simple online tool to fix your Persian subtitle encodings">
+    <meta name="keywords" content="Subtitle, Encoding, Persian, Fix, Online">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="index,follow">
+    <link rel="icon" href="favicon.png">
+    <link rel="apple-touch-icon" href="favicon.png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
 
-require('../vendor/autoload.php');
+<main class="container mt-5 text-center">
+    <div class="row">
+        <div class="col-md-6 offset-md-3">
+            <h1 class="display-5">Subfix</h1>
+            <p class="lead">Online Persian Subtitle Fixer</p>
 
-$app = new Silex\Application();
-$app['debug'] = true;
+            <div class="card mt-5 bg-light text-left">
+                <div class="card-body">
+                    <form class="d-grid gap-2 m-3">
+                        <input type="file" class="form-control" name="file">
+                        <input type="submit" class="btn btn-primary" value="Upload And Fix">
+                    </form>
+                </div>
+            </div>
 
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
+            <p class="mt-5 text-muted small">
+                &copy; <?php echo date('Y') ?> by
+                <a href="https://miladrahimi.com">Milad Rahimi</a>
+            </p>
+        </div>
+    </div>
+</main>
 
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
 
-// Our web handlers
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"></script>
 
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
+</body>
+</html>
 
-$app->run();

354316c4d046cdec2bb1b9a0308c46289e00c857
Milad Rahimi <info@miladrahimi.com>
2021-07-11 17:55:23 +0430

    update

--- web/process.php
+++ web/process.php
@@ -7,7 +7,7 @@ $content = iconv('windows-1256', 'utf-8', file_get_contents($path));
 $dir = __DIR__ . '/subtitles/';
 
 do {
-    $name = time() . random_int(10000, 99999) . '.srt';
+    $name = md5(time() . random_int(10000, 99999)) . '.srt';
 } while (file_exists($dir . $name));
 
 file_put_contents($dir . $name, $content);
